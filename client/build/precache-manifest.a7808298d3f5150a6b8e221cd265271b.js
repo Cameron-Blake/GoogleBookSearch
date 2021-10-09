b328ff03429147c7e16518e842cf63748ab2ceb7
Cameron <cameron.blake465@gmail.com>
2021-10-08 22:43:29 -0400

    fixing broken code

---
+++ client/build/precache-manifest.a7808298d3f5150a6b8e221cd265271b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c7190d17739921a28f7b198897ce84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eff6f1ec882c73a93b21",
+    "url": "/static/css/main.b8be259c.chunk.css"
+  },
+  {
+    "revision": "15372f40ab9287c8465f",
+    "url": "/static/js/2.7c85fc8a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7c85fc8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff6f1ec882c73a93b21",
+    "url": "/static/js/main.8ae37b8c.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "c0ddb6f0318a58a7089b6f7cd850a6c8",
+    "url": "/static/media/library.c0ddb6f0.jpg"
+  }
+]);
