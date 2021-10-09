03eda94a54cf388bbf5a535d25c67e25e7154759
Cameron <cameron.blake465@gmail.com>
2021-10-08 20:07:47 -0400

    wokred on client folder changes

---
+++ client/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default {
+    // Retrieves Google books
+    googleBooks: function(query) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+    },
+    // Saves book to DB
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    },
+    // saved books from DB
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+    // book with the given ID
+    getBook: function(id) {
+        return axios.get("/api/books/" + id);
+    },
+    // Deletes book with given ID
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    }
+};
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,11 +13,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 });
 
 app.listen(PORT, () => {
